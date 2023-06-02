d2343d464ab2512d0277e07b65a663d974a9396a
Praketa Saxena <praketasaxena@gmail.com>
2023-06-02 16:46:54 -0400

    update

--- 01_scalaPS_eval/src/scala/Driver.scala
+++ 01_scalaPS_eval/src/scala/Driver.scala
@@ -1,21 +1,20 @@
-
 import scala.slick.driver.JdbcProfile
 import scala.slick.driver.MySQLDriver
 
-
 object Driver {
+    val DEV = "dev"
+    val SIT = "sit"
+    val SAT = "sat"
+    val PROD = "prod"
 
-val DEV = "dev"
-val SIT = "sit"
-val SAT = "sat"
-val PROD = "prod"
-
-def getDriver: JdbcProfile = {
-    scala.util.Properties.envOrElse("runMode", "prod") 
-    match {
-        case DEV => MySQLDriver
-        case SIT => MySQLDriver
-        case SAT => MySQLDriver
-        case PROD => MySQLDriver
-        }}
+    def getDriver: JdbcProfile = 
+        {
+            scala.util.Properties.envOrElse("runMode", "prod") 
+            match {
+                case DEV => MySQLDriver
+                case SIT => MySQLDriver
+                case SAT => MySQLDriver
+                case PROD => MySQLDriver
+                }
+        }
 }
